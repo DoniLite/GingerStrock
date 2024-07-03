6727c36910450b38853eea2a649fc1efe82c56d0
DoniLite <yaomessan13@outlook.com>
2024-07-03 11:16:12 +0000

    new

---
+++ src/main/java/com/oroscop/Gingerstrock/GingerstrockApplication.java
@@ -0,0 +1,13 @@
+package com.oroscop.Gingerstrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GingerstrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GingerstrockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oroscop/Gingerstrock/GingerstrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oroscop.Gingerstrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GingerstrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
